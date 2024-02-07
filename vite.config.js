fe8a594b3f7bfee671070efa015a0c7db652eb8a
Stim Fry Mac <timothy.atoyo@gmail.com>
2024-02-07 00:06:52 +0000

    Adds prittier config to fix deployment error on build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),'prettier', 'eslint-plugin-prettier'],
+  plugins: [react(),
+    {
+      name: 'eslint',
+      enforce: 'pre', // Enforces linting before serving the app
+      apply: 'serve', // Applies linting only during development (serve mode)
+      options: {
+        lintDirtyModulesOnly: true, // Only lints files that have changed
+      },
+    },
+    {
+      name: 'prettier',
+      apply: 'build', // Applies formatting only during build
+    },
+  ],
   server: {
     open: true,
     port: 3000
